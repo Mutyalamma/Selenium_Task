ba982afb956864accd21fc295539a06d7e4a33ab
Mutyalamma <100764623+Mutyalamma@users.noreply.github.com>
2023-07-11 12:59:04 +0530

    Update loginpage.java

--- Orange HRM/Page Objects/loginpage.java
+++ Orange HRM/Page Objects/loginpage.java
@@ -1,1 +1,38 @@
+package OrangeHRMPages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import Helpers.helpers;
+
+
+public class Loginpage extends helpers
+{	
+	WebDriver driver;
+	
+	public Loginpage(WebDriver wd)
+	{
+	super(wd);
+	this.driver = wd;
+	PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath = "//*[@name='txtUsername']" )
+	WebElement username;
+	
+	@FindBy(xpath = "//*[@id='txtPassword']")
+	WebElement password;
+
+	By Login = By.xpath("//*[@type='submit']");
+	
+	public void login(String name, String pwd)
+	{
+	username.sendKeys(name);
+	password.sendKeys(pwd);
+	waitForElementToAppearAndClick(Login);
+	}
+	
+}
